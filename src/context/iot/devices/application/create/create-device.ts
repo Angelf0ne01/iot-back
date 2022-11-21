9dbfde6852a48904d10457e255abe72c05eb612c
Angel Vazquez <angel.vazquez.dev@gmail.com>
2022-11-21 13:46:39 -0300

    add all entities

--- src/context/iot/devices/application/create/create-device.ts
+++ src/context/iot/devices/application/create/create-device.ts
@@ -1,23 +1,23 @@
-
-import { DeviceId } from "context/iot/devices/domain/DeviceId";
-import { DeviceName } from "context/iot/devices/domain/DeviceName";
-import { DeviceUserId } from "context/iot/devices/domain/DeviceUserId";
-import { Device } from "../../domain/Device";
-import { DeviceIdType } from "../../domain/DeviceIdType";
-import { DeviceRepository } from "../../domain/DeviceRepository";
-
+import { TypeId } from "@context/iot/type/domain/type-id";
+import { UserId } from "@context/iot/users/domain/UserId";
+import { Device } from "../../domain/device";
+import { DeviceRepository } from "../../domain/device-repository";
+import { DeviceId } from "../../domain/devices-id";
+import { DeviceName } from "../../domain/devices-name";
+import { DeviceNumberSerial } from "../../domain/devices-number-serial";
 
 export class CreateDevice {
-    private constructor(private readonly deviceRepository: DeviceRepository) {}
-  
-    async run(
-      id: DeviceId,
-      deviceName: DeviceName,
-      idType : DeviceIdType,
-      userId : DeviceUserId,
+  private constructor(private readonly deviceRepository: DeviceRepository) { }
 
-      ): Promise<void> {
-      const device = Device.create(id, deviceName, userId, idType);
-      this.deviceRepository.save(device);
-    }
+  async run(
+    id: DeviceId,
+    deviceName: DeviceName,
+    numberSerial: DeviceNumberSerial,
+    idType: TypeId,
+    userId: UserId,
+
+  ): Promise<void> {
+    const device = Device.create(id, deviceName, numberSerial, userId, idType);
+    this.deviceRepository.save(device);
   }
+}
